fb983a78f41e9b8d37cb1fec348cb255315cb462
Gabriella Vieira <gabvi2015@outlook.com>
2023-08-22 19:40:38 -0300

    feat: req 1 - Race class

---
+++ src/Races/Race.ts
@@ -0,0 +1,26 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  // private static racesCount = 0;
+  // private _maxRaces: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race.createdRacesInstances();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+  
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints():number;
+}
